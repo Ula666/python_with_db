10d106aebd9b71c2c56ddb5cfd9259a02fb02d09
rrose666 <uziolko@hotmail.com>
2021-03-30 18:01:40 +0100

    updated

--- books.py
+++ books.py
@@ -1,31 +1,24 @@
 import requests
 
+
 class NewBooks:
     def __init__(self):
         self.url = "https://openlibrary.org/books/"
         self.greetings()
         self.books = input("Please enter book id that you're interested in: ")
-       # self.url_arg = self.url + self.books
-        #self.retrieve_info()
+    # use this id to check: OL7353617M
+
 
     def greetings(self):
         print("Hello User!")
 
-
-
-# OL7353617M
-
-
-    # def retrieve_info(self):
-    #     books = input("Please enter book id that you're interested in: ")
-    #     print(self.url_arg)
 
     def check_status(self):
         url_arg = self.url + self.books
         print(url_arg)
         status = requests.get(url_arg).status_code
         if status == 200:
-            print("The website is running" +str(status))
+            print("The website is running, and the status code is: " + str(status))
         else:
             print("Something wrong")
 
@@ -33,6 +26,3 @@ class NewBooks:
 book = NewBooks()
 book.greetings()
 book.check_status()
-
-
-
