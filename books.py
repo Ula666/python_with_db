d8a75e14d6705d0a0490ee87fea8513c2dd42bb0
rrose666 <uziolko@hotmail.com>
2021-03-30 16:23:19 +0100

    updated

--- books.py
+++ books.py
@@ -2,9 +2,13 @@ import requests
 
 class NewBooks:
     def __init__(self):
+        self.greetings()
+        self.retrieve_info()
+
         pass
 
-
+    def greetings(self):
+        print("Hello User!")
 
 
 
@@ -14,17 +18,28 @@ class NewBooks:
         #response_keys = response_dict["result"]
 
     def retrieve_info(self):
-        url = "https://openlibrary.org/books/"
-        books = input("Please enter book id ")
+        #url = "https://openlibrary.org/books/"
+        books = input("Please enter book id that you're interested in: ")
         url_arg = url + books
         # to check status code
         response = requests.get(url_arg)
         print(response.status_code)
         print(url_arg)
 
-    def display_info(self):
-        pass
+    def check_status(self):
+        try:
+            response = requests.get("https://openlibrary.org/books/")
+            if response.status_code == 200:
+                print("You can see the website")
+                print(response.json())
+            else:
+                print("something wrong" + str(response.status_code))
+        except Exception as e:
+            raise("Error")
 
+
+if __name__=='__main__':
+    obj = NewBooks()
 
 
     # for items in response_dict.keys():
